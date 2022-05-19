05b9b08764318309046b322dead9aa587e4b595f
Shubhojeet Bera <81776711+shubho0908@users.noreply.github.com>
2022-05-19 15:38:41 +0530

    form validation

---
+++ Form Validation/app.js
@@ -0,0 +1,100 @@
+let username = document.getElementById("name");
+let email = document.getElementById("email");
+let phone = document.getElementById("phone");
+let alert = document.getElementById("alert");
+let address = document.getElementById("address");
+let mssg = document.getElementById("mssg");
+
+let userValid = "";
+let mailValid = "";
+let phoneValid = "";
+
+username.addEventListener("blur", () => {
+  let user = /^[a-zA-Z]([0-9a-zA-Z]){2,10}$/;
+  let str = username.value;
+  if (user.test(str)) {
+    console.log("Your username is valid");
+    username.classList.remove("is-invalid");
+    userValid = true;
+  } else {
+    console.log("Your username is invalid");
+    username.classList.add("is-invalid");
+    userValid = false;
+  }
+});
+
+email.addEventListener("blur", () => {
+  let mail = /^([a-z\_\-\.A-Z0-9]+)@([a-zA-z]+)\.([a-zA-Z]){2,6}$/;
+  let str = email.value;
+  if (mail.test(str)) {
+    console.log("Your email is valid");
+    email.classList.remove("is-invalid");
+    mailValid = true;
+  } else {
+    email.classList.add("is-invalid");
+    mailValid = false;
+  }
+});
+
+phone.addEventListener("blur", () => {
+  let mobile = /^([0-9]){10}$/;
+  let str = phone.value;
+  if (mobile.test(str)) {
+    console.log("Your phone is valid");
+    phone.classList.remove("is-invalid");
+    phoneValid = true;
+  } else {
+    console.log("Your phone is invalid");
+    phone.classList.add("is-invalid");
+    phoneValid = false;
+  }
+});
+
+let submit = document.getElementById("submit");
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (userValid == false || mailValid == false || phoneValid == false) {
+    alert.innerHTML = `<div
+    id="success"
+    class="alert alert-danger alert-dismissible fade show"
+    role="alert"
+  >
+    <strong>Error!</strong> We couldn't submit this form, Please try again!
+    <button
+      type="button"
+      class="btn-close"
+      data-bs-dismiss="alert"
+      aria-label="Close"
+    ></button>
+  </div>`;
+    setTimeout(function () {
+      alert.innerHTML = "";
+    }, 4000);
+  } else {
+    alert.innerHTML = `<div
+    id="success"
+    class="alert alert-success alert-dismissible fade show"
+    role="alert"
+  >
+    <strong>Success!</strong> Your form has been submitted successfully!
+    <button
+      type="button"
+      class="btn-close"
+      data-bs-dismiss="alert"
+      aria-label="Close"
+    ></button>
+    </div>`;
+    setTimeout(function () {
+      alert.innerHTML = "";
+    }, 4000);
+    username.value = "";
+    email.value = "";
+    phone.value = "";
+    mssg.value = "";
+    address.value = "";
+    setTimeout(function () {
+      location.reload();
+    }, 4000);
+  }
+});
